86dc235665a1dd160a2c2c5ca34cab4bf79d06f4
Ivan_M <opana1988@yandex.ru>
2020-08-15 14:05:59 +0300

    compele exercise with test class

---
+++ src/main/java/Lesson7/Anatations/MarkingTest.java
@@ -0,0 +1,13 @@
+package Lesson7.Anatations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface MarkingTest {
+    int value() default 5;
+}
--- src/main/java/Lesson7/MainClass.java
+++ src/main/java/Lesson7/MainClass.java
@@ -1,5 +1,9 @@
 package Lesson7;
 
+import Lesson7.Anatations.MarkingTest;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class MainClass {
@@ -8,23 +12,42 @@ public class MainClass {
     public static void main(String[] args) throws Exception {
 
         // проверка заполнения массива
-        printFillArray(4,4);
-        System.out.println("-------------------------------");
-        printFillArray(5,5);
-        System.out.println("-------------------------------");
-        printFillArray(4,8);
-        System.out.println("-------------------------------");
-        printFillArray(8,4);
-        System.out.println("-------------------------------");
-        printFillArray(3,9);
-        System.out.println("-------------------------------");
-        printFillArray(9,3);
-        System.out.println("-------------------------------");
-        printFillArray(3,8);
-        System.out.println("-------------------------------");
-        printFillArray(4,9);
-        System.out.println("-------------------------------");
+//        printFillArray(4,4);
+//        System.out.println("-------------------------------");
+//        printFillArray(5,5);
+//        System.out.println("-------------------------------");
+//        printFillArray(4,8);
+//        System.out.println("-------------------------------");
+//        printFillArray(8,4);
+//        System.out.println("-------------------------------");
+//        printFillArray(3,9);
+//        System.out.println("-------------------------------");
+//        printFillArray(9,3);
+//        System.out.println("-------------------------------");
+//        printFillArray(3,8);
+//        System.out.println("-------------------------------");
+//        printFillArray(4,9);
+//        System.out.println("-------------------------------");
 
+
+        Class  classCat = Cat.class;
+        TestingClass.star(classCat);
+
+
+
+//        Class myClass = Cat.class;
+//        Field[] fields = myClass.getDeclaredFields();
+//        Method[] methods = myClass.getDeclaredMethods();
+//        for(Field o: fields){
+//            System.out.println("Type and name:  " + o.getType().getName() + "  "+ o.getName());
+//        }
+//
+//        for (Method m :
+//                methods) {
+//            if ( m.getAnnotation(MarkingTest.class) != null )
+//            System.out.println(m.getReturnType() + " || " +
+//                    m.getName() + " || " + Arrays.toString(m.getParameterTypes()));
+//        }
     }
 
     static void printFillArray( int width, int height){
